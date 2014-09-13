9cc20452d0e0ca27f8a11d7a5c34af2db83e1d04
codematix <codematix@intuitivcloud.com>
2014-09-13 14:00:27 +0530

    configs are always prefixed by base names, add malformed error handling

--- test/configurator.test.js
+++ test/configurator.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var _ = require('lodash'),
-    expect = require('chai').expect,
+var expect = require('chai').expect,
     context = describe,
     configurator;
 
@@ -12,6 +11,7 @@ process.env.TIMEOUT = 4000;
 configurator =  require('..');
 
 describe('Configurator', function () {
+  /* jshint expr: true */
 
   context('when not initialized with a base configuration path', function () {
     before(function () {
@@ -43,25 +43,39 @@ describe('Configurator', function () {
       }).to.throw('one or more base names must be specified');
     });
 
-    it('must return a promise to load a config and reject if config does not exist', function (done) {
+    it('must invoke callback with an error if config does not exist', function (done) {
       configurator.getConfig('foo', function (err) {
         expect(err.message).to.be.eq('Unable to find any configuration for \'foo\'');
         done();
       });
     });
 
-    it('must return a promise to load a config and resolve it if config found & loaded', function (done) {
+    it('must invoke callback with an error if one or more config do not exist', function (done) {
+      configurator.getConfig('conf1', 'bar', function (err) {
+        expect(err.message).to.be.eq('Unable to find any configuration for \'bar\'');
+        done();
+      });
+    });
+
+    it('must invoke callback with an error if one or more configs are malformed', function (done) {
+      configurator.getConfig('conf1', 'conf4', function (err) {
+        expect(err.message).to.be.eq('Malformed configuration file "conf4.json": Unexpected token }');
+        done();
+      });
+    });
+
+    it('must invoke callback with the config if found & loaded', function (done) {
       configurator.getConfig('conf2', function (err, config) {
         expect(err).to.be.null;
-        
-        expect(config.baseName).to.be.eq('conf2');
-        expect(config.get('a.b.c')).to.be.eq('Horray!');
+
+        expect(config.conf2.baseName).to.be.eq('conf2');
+        expect(config.conf2.get('a.b.c')).to.be.eq('Horray!');
 
         done();
       });
     });
 
-    it('must return a promise to load multiple configs and resolve it if some of them found & loaded', function (done) {
+    it('must invoke callback with multiple configs if they are found & loaded', function (done) {
       configurator.getConfig('conf1', 'conf2', function (err, config) {
         expect(err).to.be.null;
 
@@ -80,15 +94,13 @@ describe('Configurator', function () {
       configurator.getConfig('conf3', function (err, config) {
         expect(err).to.be.null;
 
-        expect(config.get('message')).to.be.eq(message);
-        expect(config.get('cfg1').url).to.be.eq(url);
-        expect(config.get('cfg1.options.timeout')).to.be.eq('4000');
+        expect(config.conf3.get('message')).to.be.eq(message);
+        expect(config.conf3.get('cfg1').url).to.be.eq(url);
+        expect(config.conf3.get('cfg1.options.timeout')).to.be.eq('4000');
 
         done();
       });
     });
-
-    it('must memoize configrations already loaded before returning them to caller');
 
   });
 
