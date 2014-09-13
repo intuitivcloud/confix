950e282f408e937bc26392d4211352ca824aefa6
codematix <codematix@intuitivcloud.com>
2014-09-13 12:47:19 +0530

    modified configuration API from promises to callbacks

--- test/configurator.test.js
+++ test/configurator.test.js
@@ -18,9 +18,15 @@ describe('Configurator', function () {
       configurator.reset();
     });
 
-    it('must throw error if get configuration attempted', function () {
+    it('must throw error if get configuration attempted without callback', function () {
       expect(function () {
         configurator.getConfig('server');
+      }).to.throw('Must invoke with a callback');
+    });
+
+    it('must throw error if get configuration attempted with callback', function () {
+      expect(function () {
+        configurator.getConfig('server', function () {});
       }).to.throw('Configurator is not initialized');
     });
   });
@@ -33,45 +39,53 @@ describe('Configurator', function () {
 
     it('must throw error if no base names are specified to get configurations', function () {
       expect(function () {
-        configurator.getConfig();
+        configurator.getConfig(function () {});
       }).to.throw('one or more base names must be specified');
     });
 
     it('must return a promise to load a config and reject if config does not exist', function (done) {
-      configurator.getConfig('foo').then(done, function (err) {
+      configurator.getConfig('foo', function (err) {
         expect(err.message).to.be.eq('Unable to find any configuration for \'foo\'');
         done();
       });
     });
 
     it('must return a promise to load a config and resolve it if config found & loaded', function (done) {
-      configurator.getConfig('conf2').then(function (config) {
+      configurator.getConfig('conf2', function (err, config) {
+        expect(err).to.be.null;
+        
         expect(config.baseName).to.be.eq('conf2');
         expect(config.get('a.b.c')).to.be.eq('Horray!');
+
         done();
-      }, done);
+      });
     });
 
     it('must return a promise to load multiple configs and resolve it if some of them found & loaded', function (done) {
-      configurator.getConfig('conf1', 'conf2').then(function (config) {
+      configurator.getConfig('conf1', 'conf2', function (err, config) {
+        expect(err).to.be.null;
+
         expect(config.conf1.baseName).to.be.eq('conf1');
         expect(config.conf2.baseName).to.be.eq('conf2');
         expect(config.conf2.get('a.b.c')).to.be.eq('Horray!');
+
         done();
-      }, done);
+      });
     });
 
     it('must replace all placeholders in string values with environment variables', function (done) {
       var message = 'Hello Jon Doe!',
           url = 'http://localhost:3753';
 
-      configurator.getConfig('conf3').then(function (config) {
+      configurator.getConfig('conf3', function (err, config) {
+        expect(err).to.be.null;
+
         expect(config.get('message')).to.be.eq(message);
         expect(config.get('cfg1').url).to.be.eq(url);
         expect(config.get('cfg1.options.timeout')).to.be.eq('4000');
 
         done();
-      }, done);
+      });
     });
 
     it('must memoize configrations already loaded before returning them to caller');
